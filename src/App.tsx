6840626e7c6ff32f0f30dacf0867ee74916a65e8
lokendra-chaulagain <lokendrachaulagain803@gmail.com>
2024-07-08 15:42:41 +0545

    translation test

--- src/App.tsx
+++ src/App.tsx
@@ -14,8 +14,41 @@ import SingleStudyAbroad from "./pages/single-study-abroad";
 import SingleTestPreparation from "./pages/single-test-preparation";
 import SingleService from "./pages/single-service";
 import SingleBlog from "./pages/single-blog";
+import { useEffect } from "react";
+
+declare global {
+  interface Window {
+    google: {
+      translate: {
+        TranslateElement: new (options: object, element: string) => void;
+      };
+    };
+    googleTranslateElementInit: () => void;
+  }
+}
 
 export default function App() {
+  const googleTranslateElementInit = () => {
+    new window.google.translate.TranslateElement(
+      {
+        // pageLanguage: "en",
+        // autoDisplay: false,
+        pageLanguage: "en",  // Set the default page language to English
+        includedLanguages: "en,es,fr,de,zh-CN", // Optional: Specify the languages you want to include
+        // layout: window.google.translate.TranslateElement.InlineLayout.SIMPLE,
+        autoDisplay: false  // Disable automatic translation dis
+      },
+      "google_translate_element"
+    );
+  };
+
+  useEffect(() => {
+    var addScript = document.createElement("script");
+    addScript.setAttribute("src", "//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit");
+    document.body.appendChild(addScript);
+    window.googleTranslateElementInit = googleTranslateElementInit;
+  }, []);
+
   return (
     <Router>
       <div className=" fixed w-full  z-50 ">
